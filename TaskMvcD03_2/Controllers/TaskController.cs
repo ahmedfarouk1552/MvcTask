8e948b52e0d85ed7350bd492a58fc89b781fc785
Ahmed Abdou <ahmedabdou1552@gmail.com>
2021-12-16 17:32:31 +0200

    first upload

---
+++ TaskMvcD03_2/Controllers/TaskController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskMvcD03_2.Controllers
+{
+    public class TaskController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult about()
+        {
+            return View();
+        }
+        public ActionResult test()
+        {
+            return View();
+        }
+        public ActionResult add()
+        {
+            return View();
+        }
+        public ActionResult contactus()
+        {
+            return View();
+        }
+    }
+}
---
+++ TaskMvcD03_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TaskMvcD03_2.MvcApplication" Language="C#" %>
